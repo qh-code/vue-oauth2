188bb3132306128b82a8f4153d807e43edba0185
Karol Laszczkowski <studiograficzne01@gmail.com>
2021-04-06 10:38:49 +0200

    config to login with cognito

--- assets/auth.js
+++ assets/auth.js
@@ -2,14 +2,25 @@
 import { OktaAuth, DEFAULT_CODE_CHALLENGE_METHOD } from '../node_modules/@okta/okta-auth-js';
 import fetchRequest from '../node_modules/@okta/okta-auth-js/lib/fetch/fetchRequest';
 // PROVIDER SPECIFIC SETTINGS (i.e. MS Identity Platform Specifics)
-const TENANT_ID = "3cf25b33-a435-44bf-ab8c-f06c50292a1e";
-const CLIENT_ID = "e8174bab-4012-41da-9ec8-6815bac754f5";
-const AUTH_DOMAIN = "https://login.microsoftonline.com/" + TENANT_ID;
-const ISSUER = AUTH_DOMAIN + "/v2.0";
+const CLIENT_ID = "6lt9cqgtupflbjc868d0i61ucv";
+const AUTH_DOMAIN = "https://vue-sample-1.auth.us-east-2.amazoncognito.com";
+// + TENANT_ID
+
+// const ISSUER = AUTH_DOMAIN + "/v2.0";
+const ISSUER = "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_CEDW1OT68";
+
 const API_SCOPE = "api://7a22a9b5-1f83-432f-8d30-ef4412294cdf/All";
-const AUTHORIZE_URL = AUTH_DOMAIN + "/oauth2/v2.0/authorize";
-const TOKEN_URL = AUTH_DOMAIN + "/oauth2/v2.0/token";
-const LOGOUT_URL = AUTH_DOMAIN + "/oauth2/v2.0/logout";
+
+const AUTHORIZE_URL = AUTH_DOMAIN + "/oauth2/authorize";
+// const AUTHORIZE_URL = AUTH_DOMAIN + "/oauth2/v2.0/authorize";
+
+// const TOKEN_URL = AUTH_DOMAIN + "/oauth2/v2.0/token";
+const TOKEN_URL = AUTH_DOMAIN + "/oauth2/token";
+
+// const LOGOUT_URL = AUTH_DOMAIN + "/oauth2/v2.0/logout";
+// const LOGOUT_URL = AUTH_DOMAIN + "/logout?client_id=" + CLIENT_ID + "&redirect_uri=http://localhost:8080&";
+const LOGOUT_URL = AUTH_DOMAIN + "/logout";
+
 let initRedirectUri = "";
 if(!process.server) {
     initRedirectUri = window.location.origin;
@@ -23,8 +34,8 @@ const oktaAuth = new OktaAuth({
     redirectUri: initRedirectUri + "/oauth-callback",
     logoutUrl: LOGOUT_URL,
     postLogoutRedirectUri: initRedirectUri,
-    // scopes: ['openid', 'profile'],
-    scopes: ['openid', 'profile', API_SCOPE],
+    scopes: ['openid', 'profile'],
+    // scopes: ['openid', 'profile', API_SCOPE],
     responseType: 'code',
     pkce: true,
     devMode: true,
@@ -113,7 +124,7 @@ export async function getUserInfo() {
         .then(idToken => { if (!idToken)
         throw new Error("not authenticated (no id_token)"); return idToken; })
         .then(idToken => idToken.claims)
-        .then(userClaims => ({ sub: userClaims.sub, name: userClaims.name, email: userClaims.email }));
+        .then(userClaims => ({ sub: userClaims.sub, name: userClaims['cognito:username'], email: userClaims.email }));
 }
 export async function oauthLogout() {
     return oktaAuth.isAuthenticated()
